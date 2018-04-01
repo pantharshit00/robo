e5294498a84cf0ea43259fd226e913777bc690f0
Harshit Pant <pantharshit00@gmail.com>
2018-04-01 11:48:58 +0530

    Add sensor distance calculation code

--- main.ino
+++ main.ino
@@ -1,12 +1,29 @@
+// Sensor Pin vars
+const int trigPin = 9;
+const int echoPin = 10;
+
+long duration;
+int distance;
+
 void setup()
 {
-  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
+  pinMode(echoPin, INPUT);  // Sets the echoPin as an Input
+  Serial.begin(9600);       // Starts the serial communication
 }
 
 void loop()
 {
-  digitalWrite(LED_BUILTIN, HIGH);
-  delay(100);
-  digitalWrite(LED_BUILTIN, LOW);
-  delay(100);
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+
+  duration = pulseIn(echoPin, HIGH);
+
+  distance = duration * 0.034 / 2; // Speed sound I am taking as 340 m/s Kartikeya give ino
+  Serial.print("Distance: ");
+  Serial.println(distance);
 }
